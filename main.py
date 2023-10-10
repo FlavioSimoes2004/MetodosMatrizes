8fec74858903c57dfe7916862e614d8608fb8705
Flávio Simões <111126287+FlavioSimoes2004@users.noreply.github.com>
2023-10-10 08:54:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
-import metGauss, metJacobi, metJordan, metSeidel
+import numpy as np
+import Seidel, Gauss, Jacobi, Jordan
 
-matriz = [[1, 2, 3], [4, 5, 6]]
+matriz = [[3.0, -2.0, 5.0, 20.0], [6.0, -9.0, 12.0, 51.0], [-5.0, 0, 2.0, 1.0]]
 
 def main():
-    print("0- metodo Gauss")
-    print("1- metodo Jacobi")
-    print("2- metodo Jordan")
-    print("3- metodo Seildel")
-    t = input()
-    while t != "0" and t != "1" and t != "2" and t != "3":
-        print("entrada invalida")
-        t = input()
-    
-    if t == "0":
-        metGauss.findValues(matriz)
-    elif t == "1":
-        metJacobi.findValues(matriz)
-    elif t == "2":
-        metJordan.findValues(matriz)
-    else: #t == 3
-        metSeidel.findValues(matriz)
-
+    printTxt = "1 - Gauss\n2 - Jordan\n3 - Jacobi\n4 - Seidel\n5 - FECHA"
+    Options = ["1", "2", "3", "4", "5"]
+    while True:
+        matrizCopy = matriz.copy()
+        print(printTxt)
+        entrada = input()
+        while entrada not in Options:
+            print("Entrada invalida\n")
+            entrada = input()
+        
+        if entrada == "1":
+            print("Gauss")
+            Gauss.gaussianElimination(matrizCopy)
+        elif entrada == "2":
+            print("Jordan")
+        elif entrada == "3":
+            print("Jacobi")
+        elif entrada == "4":
+            print("Seidel")
+            Seidel.solucao(matrizCopy)
+        else:
+            print("FECHANDO")
+            break
+        print("")
 
 main()
-print("")
