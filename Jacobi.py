490cc12bc2c506f73b8736ed51f4339930b0ec7e
Flávio Simões <111126287+FlavioSimoes2004@users.noreply.github.com>
2023-10-10 08:56:33 -0300

    Update and rename metJacobi.py to Jacobi.py

---
+++ Jacobi.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def solucao(matriz):
+    print("ok")
+
+def gauss_jacobi(A, b, x0, max_iterations=100, tolerance=10e-5):
+    n = len(A)
+    x = np.array(x0, dtype=float)
+    x_new = np.zeros_like(x)
+    
+    for iteration in range(max_iterations):
+        for i in range(n):
+            sum_ax = np.dot(A[i, :i], x[:i]) + np.dot(A[i, i+1:], x[i+1:])
+            x_new[i] = (b[i] - sum_ax) / A[i, i]
+        
+        if np.allclose(x_new, x, atol=tolerance):
+            print(f"Convergiu depois de {iteration + 1} iterações.")
+            return x_new
+        
+        x = x_new.copy()
+    
+    print("Não convergiu dentro do número especificado de iterações.")
+    return x_new
+
+# Exemplo de uso
+A = np.array([[10, 3, -2],
+              [2, 8, -1],
+              [1, 1, 5]], dtype=float)
+b = np.array([57, 20, -4], dtype=float)
+x0 = [0, 0, 0]
+
+#solution = gauss_jacobi(A, b, x0)
+#print("Solução:", solution)
